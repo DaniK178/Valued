27c3972d6ec46111378d8073d2e4a52f8f6ba0a0
Danielle Kyere <danik178@192.168.1.111>
2023-10-15 15:06:00 +0100

    added store user prompts methods

--- demo/src/main/java/com/valuedbnta/demo/controller/CustomBotController.java
+++ demo/src/main/java/com/valuedbnta/demo/controller/CustomBotController.java
@@ -1,6 +1,7 @@
 package com.valuedbnta.demo.controller;
 
 import com.valuedbnta.demo.Models.Employee;
+//import com.valuedbnta.demo.Services.PromptService;
 import com.valuedbnta.demo.Services.PromptService;
 import com.valuedbnta.demo.dto.ChatGPTRequest;
 import com.valuedbnta.demo.dto.ChatGPTResponse;
@@ -27,13 +28,22 @@ public class CustomBotController {
     @Autowired
     private RestTemplate template;
 
+    private String recommendationSetup;
+
+    private PromptService promptService;
+
+    //TO do - create a prompt to use for all recommendations:
+
     //this get mapping - takes in a prompt from the user - it return the chat gpt response
+    //we need to add a default prompt for this - before chatgpt can ask questions
     //we dont
     @GetMapping("/chat")
     public String chat(@RequestParam ("prompt") String prompt){
         //this request need a model and prompt, we input the prompt from above, we can hard code the model but lets not do this
 
-        StoreUserPrompt(prompt);
+        promptService.storeUserPrompt(prompt);
+        // get mapping endpoint to get social recommendations
+
         ChatGPTRequest request=new ChatGPTRequest(model, prompt);
        ChatGPTResponse chatGPTResponse = template.postForObject(apiURL,request,ChatGPTResponse.class);
        //this gets the content of the string
@@ -43,45 +53,46 @@ public class CustomBotController {
     //we could also return the entire object using ChatGPTResponse instead of String
 
     //ALL OF THESE METHODS BELOW NEED AN INPUT OF THE USERS SENT MESSAGES- to send to chat gpt to analyse
-
-    // get mapping endpoint to get social recommendations
-
     //messages from a particular user will be saved to a database
     //maybe access the message by getting the user id - and retrieving the messsages from this
 
-    private PromptService promptService;
+    // get mapping endpoint to get social recommendations
 
-    @GetMapping("/get-prompt")
+
+    @GetMapping("/get-social-recommendations")
     public String getAndSendStoredPrompts() {
         // Retrieve the stored prompts from our service/database
         List<String> storedPrompts = promptService.getStoredPrompts();
-        // Make a single prompt string from the collected prompts
         String combinedPrompt = String.join("\n", storedPrompts);
+        String socialRequest = "Hello, from now on you will be a workplace chatbox that is supportive and gives recommendations to the employe based on the prompts listed below, the recommendation must fall under these three categories: \"social\", \"learning and development\" and \"disability support\".\n" +
+                "I will be the employee. Please list up to 7 social recommendations you would make from the following prompts. Display them in a numbered list " + combinedPrompt;
+        ChatGPTRequest combinedPromptRequest = new ChatGPTRequest(model, socialRequest);
+        ChatGPTResponse socialGPTResponse = template.postForObject(apiURL,combinedPromptRequest,ChatGPTResponse.class);
+
         // Make an API request to ChatGPT using the combined prompt
-        String gptResponse = makeChatGPTRequest(combinedPrompt);
+//        String gptResponse = makeChatGPTRequest(combinedPrompt);
+//        return gptResponse;
 
-        return gptResponse;
+        return socialGPTResponse.getChoices().get(0).getMessage().getContent();
     }
 
-    //or return all of  chat gpt response?
-    //I want to get to get the prompt from one chatgpt and pass this as a param
-    //do i collect the same prompt object??
+
+    //you are an workplace bot
 
 
 
 
 
 
-
-    @GetMapping("/chat/social")
-    public String socialRecommendations (@RequestParam (defaultValue = "Hello, from now on you will be a workplace chatbox that gives recommendations to the employee. From the conversations, the recommendation must fall under these three categories: \"social\", \"learning and development\" and \"disability support\".\n" +
-            "I will be the employee\n, If you are to give any recommendation in your response, please state that category it falls under at the start of the response by putting e.g.Social category or Disability support category. The conversations are listed below" + usersMessages) String prompt){
-        //this request need a model and prompt, we input the prompt from above, we can hard code the model but lets not do this
-        ChatGPTRequest request=new ChatGPTRequest(model, prompt);
-        ChatGPTResponse chatGPTResponse = template.postForObject(apiURL,request,ChatGPTResponse.class);
-        //this gets the content of the string
-        return chatGPTResponse.getChoices().get(0).getMessage().getContent();
-    }
+//    @GetMapping("/chat/social")
+//    public String socialRecommendations (@RequestParam (defaultValue = "Hello, from now on you will be a workplace chatbox that gives recommendations to the employee. From the conversations, the recommendation must fall under these three categories: \"social\", \"learning and development\" and \"disability support\".\n" +
+//            "I will be the employee\n, If you are to give any recommendation in your response, please state that category it falls under at the start of the response by putting e.g.Social category or Disability support category. The conversations are listed below" + usersMessages) String prompt){
+//        //this request need a model and prompt, we input the prompt from above, we can hard code the model but lets not do this
+//        ChatGPTRequest request=new ChatGPTRequest(model, prompt);
+//        ChatGPTResponse chatGPTResponse = template.postForObject(apiURL,request,ChatGPTResponse.class);
+//        //this gets the content of the string
+//        return chatGPTResponse.getChoices().get(0).getMessage().getContent();
+//    }
 
     //get mapping endpoint to get l&D recommendations
 
