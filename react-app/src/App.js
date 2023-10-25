23f35136f5face55a67e81dc59956b38ef384a1d
Hamza Abdilahi <85694436+hamza-abdilahi@users.noreply.github.com>
2023-10-25 20:33:20 +0100

    insights and tips

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react';
+
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/NavBar';
 import Footer from './components/Footer';
@@ -10,6 +12,40 @@ import './App.css';
 import Settings from './components/Settings';
 
 function App() {
+
+  const [socialRecommendations, setSocialRecommendations] = useState('');
+  const [learningRecommendations, setLearningRecommendations] = useState('');
+  const [disabilityRecommendations, setDisabilityRecommendations] = useState('');
+
+  useEffect(() => {
+    // Fetch social recommendations
+    fetch(`http://localhost:8080/bot/get-social-recommendations`)
+      .then((response) => response.text())
+      .then((data) => {
+        setSocialRecommendations(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching social recommendations:', error);
+      });
+    //  learning recommendations
+    fetch(`http://localhost:8080/bot/get-learning-recommendations`)
+      .then((response) => response.text())
+      .then((data) => {
+        setLearningRecommendations(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching learning recommendations:', error);
+      });
+    //  disability recommendations
+    fetch(`http://localhost:8080/bot/get-disability-recommendations`)
+      .then((response) => response.text())
+      .then((data) => {
+        setDisabilityRecommendations(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching disability recommendations:', error);
+      });
+  }, []);
   return (
   <Router>
     <div className="App">
@@ -26,7 +62,11 @@ function App() {
             <Chat/>
           </Route>
           <Route path="/tips">
-            <Tips />
+          <Tips
+            socialRecommendations={socialRecommendations}
+            disabilityRecommendations={disabilityRecommendations}
+            learningRecommendations={learningRecommendations}
+          />
           </Route>
           <Route path="/insights">
             <Insights />
