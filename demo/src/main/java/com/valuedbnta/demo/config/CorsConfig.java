689c01a82fefa76a59c93229fabb190c0e03446e
Hamza Abdilahi <85694436+hamza-abdilahi@users.noreply.github.com>
2023-10-25 10:12:00 +0100

    cors config

---
+++ demo/src/main/java/com/valuedbnta/demo/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.valuedbnta.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000/Chat"); // Replace with your React app's URL
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
